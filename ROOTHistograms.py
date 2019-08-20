24cd41aced50d2d7b0f1bbe8f8083d59d63bc176
lopezzot <lorenzo.pezzotti01@gmail.com>
2019-08-20 08:50:15 +0200

    beforehomepc

--- ROOTHistograms.py
+++ ROOTHistograms.py
@@ -12,7 +12,48 @@ def create_eventdisplay_scin(PrimaryParticleName, VectorSignalsR, VectorSignalsL
 	deltatheta = 45./(NbOfBarrel)
 
 	#Set ROOT histograms (x=theta, y=phi)
-	TH2Signals = TH2F("ScinSignals_"+histoname, PrimaryParticleName, NbOfBarrel*2, -1*(deltatheta*NbOfBarrel+deltatheta/2), deltatheta*NbOfBarrel+deltatheta/2, NZrot, 0., 360.)
+	TH2Signals = TH2F("ScinSignals_"+histoname, PrimaryParticleName, (NbOfBarrel+NbOfEndcap)*2, -1*(deltatheta*(NbOfBarrel+NbOfEndcap)+deltatheta/2), deltatheta*(NbOfBarrel+NbOfEndcap)+deltatheta/2, NZrot, 0., 360.)
+	
+	#Fill histograms in for loop
+	for towerindex in range(TotTower*NZrot):
+		theta, phi = newmap.maptower(towerindex, "right")
+		TH2Signals.Fill(theta,phi, VectorSignalsR[towerindex])
+		theta, phi = newmap.maptower(towerindex, "left")
+		TH2Signals.Fill(theta, phi, VectorSignalsL[towerindex])
+
+	#Draw + DrawOptions histograms	
+	Style = gStyle
+	Style.SetPadRightMargin(0.16)
+	Style.SetPalette(1) #Root palette style
+	Style.SetOptStat(0) #Do not show statistics
+	TH2Signals.SetLineWidth(0) #TH2Signals #No line width
+	TH2Signals.SetLineColor(2)
+	#TH2Signals.SetFillColorAlpha(2, 0.)
+	XAxis = TH2Signals.GetXaxis()
+	XAxis.SetTitle("Theta (deg)")
+	XAxis.CenterTitle()
+	#XAxis.SetTitleOffset(1.8)
+	YAxis = TH2Signals.GetYaxis()
+	YAxis.SetTitle("Phi (deg)")
+	YAxis.CenterTitle()
+	#YAxis.SetTitleOffset(1.8)
+	ZAxis = TH2Signals.GetZaxis()
+	#ZAxis.SetTitle("Energy (MeV)")
+	#ZAxis.SetTitleOffset(1.4)
+	TH2Signals.Draw("COLZ 0 FB")
+	TH2Signals.Write()
+
+def create_eventdisplay_cher(PrimaryParticleName, VectorSignalsR, VectorSignalsL, histoname):
+	"""Function to perform ROOT event display form calo"""
+	NbOfBarrel=40
+	NbOfEndcap=35
+	NZrot=36
+	TotTower=NbOfBarrel+NbOfEndcap
+
+	deltatheta = 45./(NbOfBarrel)
+
+	#Set ROOT histograms (x=theta, y=phi)
+	TH2Signals = TH2F("CherSignals_"+histoname, PrimaryParticleName, (NbOfBarrel+NbOfEndcap)*2, -1*(deltatheta*(NbOfBarrel+NbOfEndcap)+deltatheta/2), deltatheta*(NbOfBarrel+NbOfEndcap)+deltatheta/2, NZrot, 0., 360.)
 	
 	#Fill histograms in for loop
 	for towerindex in range(TotTower*NZrot):
--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 import map
 import ROOTHistograms
-from ROOT import TTree, TFile, TH1F, TGraph, TGraphErrors
+from ROOT import TTree, TFile, TH1F, TGraph, TGraphErrors, gStyle
 import glob
 from array import array
 import os
@@ -15,7 +15,7 @@ if machine == "linux":
 		path = str("/home/lorenzo/cernbox/work/Git-to-Mac/AnalysisIDEACalorimeter/")
 		datapath = str("/home/lorenzo/cernbox/work/Git-to-Mac/IDEA_Calorimeter_Union_data/BarrelR/")
 if machine == "office":
-		datapath = str("/home/software/Calo/results/tower_calibration/")
+		datapath = str("/home/software/Calo/results/NewTowerScan/")
 
 def eventdisplay(inputfile, outputfile, histoname):
 	#inputfile = raw_input("Insert root file: ")
@@ -61,18 +61,38 @@ def towercalibration():
 	Zeros = array('d')
 	errorsscin = array('d')
 	errorscher = array('d')
+	Energy = array('d')
+
+	ResponseMeanScin = array('d')
+	ResponseMeanCher = array('d')
+	ResponseRMSScin = array('d')
+	ResponseRMSCher = array('d')
+	ResponseZeros = array('d')
+	Responseerrorsscin = array('d')
+	Responseerrorscher = array('d')
+	energytot = array('d')
+	scinsignaltot = array('d')
+	chersignaltot = array('d')
+	ScinSignalTot = array('d')
+	
 
 	inputfiles = sorted(glob.glob(datapath+"*"), key=os.path.getmtime) #get files from tower 1 to 75 ordered by creation time
-	
+	#inputfiles = ["/home/software/Calo/Calibrations/Calib_"+str(i)+".root" for i in range(1,76)]
 	for counter, inputfile in enumerate(inputfiles):
 		inputfile = TFile(inputfile)
-		print str(inputfile)+" \n"
+		print "Analyzing: "+str(inputfile)+" \n"
 		tree = TTree()
 		inputfile.GetObject("B4", tree)	
 
-		ScinHist = TH1F("scin_", str(counter+1)+"_scin", 100, 0., 10.)
-		CherHist = TH1F("cher_", str(counter+1)+"_cher", 100, 0., 50.)	
-		EnergyTowerHist = TH1F("Energy_",str(counter+1)+"_Energy", 200, 0., 100000.)
+		ScinHist = TH1F("scin_", str(counter+1)+"_scin", 1000, 0., 1000.)
+		CherHist = TH1F("cher_", str(counter+1)+"_cher", 600, 0., 600.)	
+		EnergyTowerHist = TH1F("Energy_",str(counter+1)+"_Energy", 200, 0., 200.)
+		EnergyHist = TH1F("E_", str(counter+1)+"_Energy", 200, 0., 200.)
+		ResponseScinHist = TH1F("responsescin_", str(counter+1)+"_scin", 1000, 0., 1000.)
+		ResponseCherHist = TH1F("responsecher_", str(counter+1)+"_cher", 600, 0., 600.)
+
+		Signalscinhist = TH1F("scintot_", str(counter+1)+"_scin", 30000, 0., 30000)
+
 		#loop over events
 		for Event in range(int(tree.GetEntries())):	
 
@@ -95,27 +115,48 @@ def towercalibration():
 
 			signalscin = max(BarrelR_VectorSignals)
 			signalcher = max(BarrelR_VectorSignalsCher)
-			energytower = max(VectorR)
+			energytower = max(VectorR)/1000
 			
-			ScinHist.Fill(energytower/signalscin)	
-			CherHist.Fill(energytower/signalcher)	
+			ScinHist.Fill(signalscin/energytower)	
+			CherHist.Fill(signalcher/energytower)	
 			EnergyTowerHist.Fill(energytower)
 
+			totalsignalscin = sum(BarrelR_VectorSignals)+sum(BarrelL_VectorSignals)
+			totalsignalcher = sum(BarrelR_VectorSignalsCher)+sum(BarrelL_VectorSignalsCher)
+			totalenergy = (sum(VectorR)+sum(VectorL))/1000
+			EnergyHist.Fill(totalenergy)
+
+
+			ResponseScinHist.Fill(totalsignalscin/totalenergy)
+			ResponseCherHist.Fill(totalsignalcher/totalenergy)
+			Signalscinhist.Fill(totalsignalscin)
+
+			energytot.append(totalenergy)
+			scinsignaltot.append(totalsignalscin)
+			chersignaltot.append(totalsignalcher)
+
 			if Event < 1:
-				print "Max found at: "+str(list(BarrelR_VectorSignals).index(signalscin))+str(list(BarrelR_VectorSignalsCher).index(signalcher))+str(list(VectorR).index(energytower))+" for file "+str(counter+1) #to check tower mostly hitten is the correct one
+				print "Max found at: "+str(list(BarrelR_VectorSignals).index(signalscin))+str(list(BarrelR_VectorSignalsCher).index(signalcher))+str(list(VectorR).index(energytower*1000))+" for file "+str(counter+1) #to check tower mostly hitten is the correct one
 				displayfile.cd()
 				ROOTHistograms.create_eventdisplay_scin(PrimaryParticleName, BarrelR_VectorSignals, BarrelL_VectorSignals, str(counter)) 
-		
+				ROOTHistograms.create_eventdisplay_cher(PrimaryParticleName, BarrelR_VectorSignalsCher, BarrelL_VectorSignalsCher, str(counter))
+
+		print np.mean(energytot), np.mean(scinsignaltot), np.mean(chersignaltot)
 		displayfile.cd()
+		gStyle.SetOptStat(111)
+		ScinHist.Fit("gaus")
+		CherHist.Fit("gaus")
 		ScinHist.Write()
 		CherHist.Write()
 		EnergyTowerHist.Write()
-		ScinHist.Fit("gaus")
-		MeanScin.append(ScinHist.GetMean())
-		MeanCher.append(CherHist.GetMean())
+		MeanScin.append(ScinHist.GetFunction("gaus").GetParameter(1))
+		MeanCher.append(CherHist.GetFunction("gaus").GetParameter(1))
 		RMSScin.append(ScinHist.GetRMS())
 		RMSCher.append(CherHist.GetRMS())
 		EnergyTower.append(EnergyTowerHist.GetMean())
+		Energy.append(EnergyHist.GetMean())
+		ScinSignalTot.append(Signalscinhist.GetMean())
+		Signalscinhist.Write()
 		#print ScinHist.GetMean(), CherHist.GetMean(), EnergyTowerHist.GetMean()
 		#print ScinHist.GetRMS(), CherHist.GetRMS()
 		Tower.append(counter+1)
@@ -123,25 +164,71 @@ def towercalibration():
 		errorsscin.append(ScinHist.GetRMS()/(3000**0.5))
 		errorscher.append(CherHist.GetRMS()/(3000**0.5))
 
-	#print np.mean(MeanScin), max(MeanScin), min(MeanScin)
-	#print np.mean(MeanCher), max(MeanCher), min(MeanCher)
-	
+		ResponseScinHist.Fit("gaus")
+		ResponseCherHist.Fit("gaus")
+		ResponseScinHist.Write()
+		ResponseCherHist.Write()
+		ResponseMeanScin.append(ResponseScinHist.GetFunction("gaus").GetParameter(1))
+		ResponseMeanCher.append(ResponseCherHist.GetFunction("gaus").GetParameter(1))
+		ResponseRMSScin.append(ResponseScinHist.GetRMS())
+		ResponseRMSCher.append(ResponseCherHist.GetRMS())
+		Responseerrorsscin.append(ResponseScinHist.GetRMS()/(3000**0.5))
+		Responseerrorscher.append(ResponseCherHist.GetRMS()/(3000**0.5))
+
 	n = len(MeanCher)
+	ScinTotGraph = TGraph(n, Tower, ScinSignalTot)
+	ScinTotGraph.SetName("ScinTotGraph")
+	ScinTotGraph.Write()
+	EnergyGraph = TGraph(n, Tower, Energy)
+	EnergyGraph.SetName("EnergyGraph")
+	EnergyGraph.Write()
+	EnergyTowerGraph = TGraph(n, Tower, EnergyTower)
+	EnergyTowerGraph.SetName("EnergyTower")
+	EnergyTowerGraph.Write()
 	RMSGraphScin = TGraph(n, Tower, RMSScin)
+	RMSGraphScin.SetName("Calibration_RMSGraphScin")
 	RMSGraphCher = TGraph(n, Tower, RMSCher)
+	RMSGraphCher.SetName("Calibration_RMSGraphCher")
 	MeanGraphScin = TGraphErrors(n, Tower, MeanScin, Zeros, errorsscin)
+	MeanGraphScin.SetName("Calibration_Scin")
 	MeanGraphCher = TGraphErrors(n, Tower, MeanCher, Zeros, errorscher)
+	MeanGraphCher.SetName("Calibration_Cher")
 	x = array('d', (0., 90., 90., 0.))
-	y = array('d', (np.mean(MeanScin)-0.015*np.mean(MeanScin), np.mean(MeanScin)-0.015*np.mean(MeanScin), np.mean(MeanScin)+0.015*np.mean(MeanScin), np.mean(MeanScin)+0.015*np.mean(MeanScin)))
+	y = array('d', (np.mean(MeanScin)-0.04*np.mean(MeanScin), np.mean(MeanScin)-0.04*np.mean(MeanScin), np.mean(MeanScin)+0.04*np.mean(MeanScin), np.mean(MeanScin)+0.04*np.mean(MeanScin)))
 	Fillgraph = TGraph(4, x, y )
+	Fillgraph.SetName("Calibration_banscin")
 	Fillgraph.Write()
 	x2 = array('d', (0., 90., 90., 0.))
-	y2 = array('d', (np.mean(MeanCher)-0.01*np.mean(MeanCher), np.mean(MeanCher)-0.01*np.mean(MeanCher), np.mean(MeanCher)+0.01*np.mean(MeanCher), np.mean(MeanCher)+0.01*np.mean(MeanCher)))
+	y2 = array('d', (np.mean(MeanCher)-0.04*np.mean(MeanCher), np.mean(MeanCher)-0.04*np.mean(MeanCher), np.mean(MeanCher)+0.04*np.mean(MeanCher), np.mean(MeanCher)+0.04*np.mean(MeanCher)))
 	Fillgraph2 = TGraph(4, x2, y2 )
+	Fillgraph2.SetName("Calibration_bancher")
 	Fillgraph2.Write()
 	MeanGraphCher.Write()
 	MeanGraphScin.Write()
 	RMSGraphCher.Write()
 	RMSGraphScin.Write()
+
+	ResponseRMSGraphScin = TGraph(n, Tower, ResponseRMSScin)
+	ResponseRMSGraphScin.SetName("ResponseRMSGraphScin")
+	ResponseRMSGraphCher = TGraph(n, Tower, ResponseRMSCher)
+	ResponseRMSGraphCher.SetName("ResponseRMSGraphCher")
+	ResponseMeanGraphScin = TGraphErrors(n, Tower, ResponseMeanScin, Zeros, Responseerrorsscin)
+	ResponseMeanGraphScin.SetName("ResponseMeanGraphScin")
+	ResponseMeanGraphCher = TGraphErrors(n, Tower, ResponseMeanCher, Zeros, Responseerrorscher)
+	ResponseMeanGraphCher.SetName("ResponseMeanGraphCher")
+	x = array('d', (0., 90., 90., 0.))
+	y = array('d', (np.mean(ResponseMeanScin)-0.04*np.mean(ResponseMeanScin), np.mean(ResponseMeanScin)-0.04*np.mean(ResponseMeanScin), np.mean(ResponseMeanScin)+0.04*np.mean(ResponseMeanScin), np.mean(ResponseMeanScin)+0.04*np.mean(ResponseMeanScin)))
+	Fillgraph = TGraph(4, x, y )
+	Fillgraph.SetName("ResponseBan_scin")
+	Fillgraph.Write()
+	x2 = array('d', (0., 90., 90., 0.))
+	y2 = array('d', (np.mean(ResponseMeanCher)-0.04*np.mean(ResponseMeanCher), np.mean(ResponseMeanCher)-0.04*np.mean(ResponseMeanCher), np.mean(ResponseMeanCher)+0.04*np.mean(ResponseMeanCher), np.mean(ResponseMeanCher)+0.04*np.mean(ResponseMeanCher)))
+	Fillgraph2 = TGraph(4, x2, y2 )
+	Fillgraph2.SetName("ResponseBan_cher")
+	Fillgraph2.Write()
+	ResponseMeanGraphCher.Write()
+	ResponseMeanGraphScin.Write()
+	ResponseRMSGraphCher.Write()
+	ResponseRMSGraphScin.Write()
 
 towercalibration()
